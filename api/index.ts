a263471c9ba0212d75723b15e2db8bbb629f9894
Lycaan <ekirwa35@gmail.com>
2026-08-30 07:44:46 +0300

    Export express app directly

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,2 @@
-import { Request, Response } from 'express';
 import { createServer } from '../server/index.js';
-
-const app = createServer();
-
-export default function handler(req: Request, res: Response) {
-  return app(req, res);
-}
+export default createServer();
